469c0dedbbd64fe8aaaa1b21e3bcbd76bc466e9d
goswamivikash <swamiv768@gmail.com>
2026-06-28 03:05:39 +0530

    Update ├── script.js

--- ├── script.js
+++ ├── script.js
@@ -1,3 +1,10 @@
-function showMessage() {
-  alert("Keep going Vikash 🚀 Your experience is already powerful!");
+function toggleTheme() {
+  document.body.classList.toggle("light");
+}
+
+function downloadResume() {
+  const link = document.createElement("a");
+  link.href = "data:text/plain;charset=utf-8,Vikash Goswami Resume";
+  link.download = "Vikash_Goswami_Resume.txt";
+  link.click();
 }
